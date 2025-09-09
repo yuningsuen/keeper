d75991d5aa66a353c6479b58d17db89166217a09
yuningsuen <yuningsuen@gmail.com>
2025-09-09 10:39:15 +0800

    v1.0

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import { Fab } from "@mui/material";
+import { Zoom } from "@mui/material";
+
+function CreateArea(props) {
+  const [isFocused, setIsFocused] = useState(false);
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+          hidden={!isFocused}
+        />
+        <textarea
+          name="content"
+          onClick={() => {
+            setIsFocused(true);
+          }}
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={isFocused ? 3 : 1}
+        />
+        <Zoom in={isFocused}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
